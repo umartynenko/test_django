6a90c20a2de9dd51016fc7f9bbb5bce0a377f94f
umartynenko <uriy.martynenko@gmail.com>
2022-09-25 22:41:05 +0300

    APIView

---
+++ article/views.py
@@ -0,0 +1,37 @@
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleView(APIView):
+    def get(self, request, *args, **kwargs):
+        article = Article.objects.all()
+        serializer = ArticleSerializer(article, many=True)
+        return Response({'articles': serializer.data})
+
+    def post(self, request):
+        article = request.data.get('articles')
+        serializer = ArticleSerializer(data=article)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({'success': 'Article "{}" created successfully'.format(
+            article_saved.title)})
+
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('articles')
+        serializer = ArticleSerializer(instance=saved_article, data=data,
+                                       partial=True)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+
+        return Response({'success': "Article '{}' update successfully".format(
+            article_saved.title)})
+
+    def delete(self, request, pk):
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+        return Response(
+            {'message': "Article with id '{}' has been deleted.".format(pk)})
